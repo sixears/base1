b1823d9cda3f55cb007ae7e0fcbe06fe94d92f1f
Mr. Fluffy <git@sixearsm>
2022-11-17 19:33:25 +0000

    upgrade to callPackage-based versions

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.10";
+    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.11";
 
-    base0.url         = "github:sixears/base0/r0.0.4.5";
-    has-callstack.url = "github:sixears/has-callstack/r1.0.1.10";
-    index.url         = "github:sixears/index/r1.0.1.10";
-    monaderror-io.url = "github:sixears/monaderror-io/r1.2.5.9";
-    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.7";
-    tfmt.url          = "github:sixears/tfmt/r0.2.7.10";
+    base0.url         = "github:sixears/base0/r0.0.4.6";
+    has-callstack.url = "github:sixears/has-callstack/r1.0.1.11";
+    index.url         = "github:sixears/index/r1.0.1.11";
+    monaderror-io.url = "github:sixears/monaderror-io/r1.2.5.10";
+    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.8";
+    tfmt.url          = "github:sixears/tfmt/r0.2.7.11";
 
   };
 
