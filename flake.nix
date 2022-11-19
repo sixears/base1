b10d09cb8865658f124bd533eac981e5549f7eba
Mr. Fluffy <git@sixearsm>
2022-11-19 21:45:24 +0000

    tasty-plus->1.5.2.22

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     base0.url         = github:sixears/base0/r0.0.4.10;
     has-callstack.url = github:sixears/has-callstack/r1.0.1.17;
-    index.url         = github:sixears/index/r1.0.1.23;
+    index.url         = github:sixears/index/r1.0.1.24;
     monaderror-io.url = github:sixears/monaderror-io/r1.2.5.18;
     more-unicode.url  = github:sixears/more-unicode/r0.0.17.11;
     tfmt.url          = github:sixears/tfmt/r0.2.7.23;
@@ -24,7 +24,7 @@
         in
           mkDerivation {
             pname = "base1";
-            version = "0.0.9.21";
+            version = "0.0.9.32";
             src = ./.;
             libraryHaskellDepends = map (p: pkg p) [
               base0 has-callstack index monaderror-io more-unicode tfmt
