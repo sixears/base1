f0abadb50fc506d02dddc689d056395388cec356
Mr. Fluffy <git@sixearsm>
2022-11-20 07:18:31 +0000

    has-callstack->1.0.1.19

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
     base0.url         = github:sixears/base0/r0.0.4.11;
-    has-callstack.url = github:sixears/has-callstack/r1.0.1.18;
-    index.url         = github:sixears/index/r1.0.1.25;
-    monaderror-io.url = github:sixears/monaderror-io/r1.2.5.19;
+    has-callstack.url = github:sixears/has-callstack/r1.0.1.19;
+    index.url         = github:sixears/index/r1.0.1.26;
+    monaderror-io.url = github:sixears/monaderror-io/r1.2.5.20;
     more-unicode.url  = github:sixears/more-unicode/r0.0.17.12;
-    tfmt.url          = github:sixears/tfmt/r0.2.7.24;
+    tfmt.url          = github:sixears/tfmt/r0.2.7.25;
 
   };
 
@@ -24,7 +24,7 @@
         in
           mkDerivation {
             pname = "base1";
-            version = "0.0.9.33";
+            version = "0.0.9.34";
             src = ./.;
             libraryHaskellDepends = map (p: pkg p) [
               base0 has-callstack index monaderror-io more-unicode tfmt
